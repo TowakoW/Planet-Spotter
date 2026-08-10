4a5f578e30d765d5e81ae0c0375466960de069f4
Towako Wunsch <towakowunsch@berkeley.edu>
2026-08-09 17:24:31 -0700

    Cleaning "horizons_api.py" for repetition

--- horizons_api.py
+++ horizons_api.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 import requests
+import sys
+import json
+from urllib.parse import urlencode
 
 # Base URL for NASA JPL Horizons API
 BASE_URL = "https://ssd.jpl.nasa.gov/api/horizons.api"
 
+# Get requested NAIF-ID from commandline:
+if (len(sys.argv)) == 1:
+    print("Please input planet NAIF-ID on command line.")
+    sys.exit(2)
+naifid = sys.argv[1:]
+
 # commands = ["199", "299", "499", "599", "699", "799", "899", "999"]
-commands = ["199"]
+# commands = ["199"]
 
+def main() -> int:
+    for cmd in naifid:
+        fetch_horizons_data(cmd)
+    return 0
 
-def fetch_horizons_data(cmd: str = "199"):
+def fetch_horizons_data(cmd):
     print(f"\n=== COMMAND {cmd} ===")
 
     params = {
@@ -19,28 +32,33 @@ def fetch_horizons_data(cmd: str = "199"):
         "START_TIME": "2026-07-20",    # Start date
         "STOP_TIME": "2026-07-21",     # End date
         "STEP_SIZE": "1d",             # Step size (daily)
-        "CSV_FORMAT": "YES"
+        # "CSV_FORMAT": "YES"
     }
 
-    response = requests.get(BASE_URL, params=params, timeout=30)
-    print("Request URL:", response.request.url)
+    #URL encode
+    query_string = urlencode(params)
+    url = f"{BASE_URL}?{query_string}"
 
+    # Submit API request and decode JSON
+    response = requests.get(url)
+    print("URL:", url)
+    try:
+        data = json.loads(response.text)
+    except ValueError:
+        print("unable to decode JSON results.")
+
+    
     if response.status_code != 200:
         raise RuntimeError(f"Error: {response.status_code}\n{response.text}")
+        sys.exit(1)
 
-    data = response.json()
     print("Request successful. Summary:")
     print(data.get("result") or data)
     return data
 
 
-data = fetch_horizons_data(commands[0])
+# data = fetch_horizons_data(naifid)
 
-
-def main() -> int:
-    for cmd in commands:
-        fetch_horizons_data(cmd)
-    return 0
 
 
 if __name__ == "__main__":
